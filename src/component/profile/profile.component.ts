679b19bb70825ec5e1ff861ac82d2b9855b63493
shivakant786 <shivakanttiwari123@gmail.com>
2024-09-20 18:10:44 +0530

    add api data on signal

--- src/component/profile/profile.component.ts
+++ src/component/profile/profile.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { JsonPipe , CommonModule} from '@angular/common';
+import { CardModule } from 'primeng/card';
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe , CardModule , CommonModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  isLoading = true; 
+  postList = signal<any[]>([]);
+  auth = inject(AuthService);
+  ngOnInit(): void {
+    this.getPost();
+  }
 
+  getPost() {
+    this.isLoading = true;
+    this.auth.getAllPost().subscribe(
+      (res: any) => {
+        this.postList.set(res);
+        this.isLoading = false;  
+      },
+      (error) => {
+        console.error('Error fetching posts', error);
+        this.isLoading = false;
+      }
+    );
+  }
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { environment } from '../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
+  http = inject(HttpClient)
   constructor() { }
+  getAllPost():Observable<any>{
+    return this.http.get(environment.API_URL+"/posts")
+  }
 }
